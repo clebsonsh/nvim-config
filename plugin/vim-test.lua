26c368758c072e5a5d5e47cb0cc79802d778b850
Clebson Moura <clebson.sh@gmail.com>
2026-04-26 18:18:00 -0300

    chore: give a description for test commands

--- plugin/vim-test.lua
+++ plugin/vim-test.lua
@@ -2,8 +2,8 @@ vim.pack.add {
   'https://github.com/vim-test/vim-test',
 }
 
-vim.keymap.set('n', '<leader>tt', '<cmd>TestNearest<CR>')
-vim.keymap.set('n', '<leader>tT', '<cmd>TestFile<CR>')
-vim.keymap.set('n', '<leader>ta', '<cmd>TestSuite<CR>')
-vim.keymap.set('n', '<leader>tl', '<cmd>TestLast<CR>')
-vim.keymap.set('n', '<leader>tg', '<cmd>TestVisit<CR>')
+vim.keymap.set('n', '<leader>tt', '<cmd>TestNearest<CR>', { desc = 'Run Current Test' })
+vim.keymap.set('n', '<leader>tf', '<cmd>TestFile<CR>', { desc = 'Run Test File' })
+vim.keymap.set('n', '<leader>ts', '<cmd>TestSuite<CR>', { desc = 'Run Test Suite' })
+vim.keymap.set('n', '<leader>tl', '<cmd>TestLast<CR>', { desc = 'Run Latest Test' })
+vim.keymap.set('n', '<leader>tv', '<cmd>TestVisit<CR>', { desc = 'Visit Test' })
