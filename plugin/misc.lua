0c5f4209938f519351065e60b260c3be8ebd526e
Clebson Moura <clebson.santos@loopbrasil.com>
2026-04-26 19:22:35 -0300

    chore: remove indent blank line

--- plugin/misc.lua
+++ plugin/misc.lua
@@ -2,10 +2,8 @@ vim.pack.add {
   'https://github.com/NMAC427/guess-indent.nvim',
   'https://github.com/windwp/nvim-autopairs',
   'https://github.com/lewis6991/gitsigns.nvim',
-  'https://github.com/lukas-reineke/indent-blankline.nvim',
   'https://github.com/folke/which-key.nvim',
 }
 
 require('guess-indent').setup()
 require('nvim-autopairs').setup()
-require('ibl').setup()
