a8ff52b3fc5c511f0c9371b414ffd91e8ee3b91e
Clebson Moura <clebson.sh@gmail.com>
2026-05-01 21:29:41 -0300

    chore: add keybind to run only selected query

--- plugin/dbui.lua
+++ plugin/dbui.lua
@@ -10,3 +10,6 @@ vim.g.db_ui_save_location = '~/db/queries'
 vim.g.db_ui_tmp_query_location = '~/db/tmp'
 
 vim.keymap.set('n', '<leader>d', '<cmd>DBUIToggle<CR>')
+
+-- Execute selected SQL in visual mode
+vim.keymap.set('v', '<leader>d', [[:DB<CR>]], { desc = 'Run selected SQL query' })
