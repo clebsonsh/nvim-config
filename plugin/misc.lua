312415520e0fcd3a8a5038f75228afc41e6fc2be
Clebson Moura <clebson.sh@gmail.com>
2026-05-06 15:37:29 -0300

    chore: add markdown viewer

--- plugin/misc.lua
+++ plugin/misc.lua
@@ -3,7 +3,12 @@ vim.pack.add {
   'https://github.com/windwp/nvim-autopairs',
   'https://github.com/lewis6991/gitsigns.nvim',
   'https://github.com/folke/which-key.nvim',
+  'https://github.com/MeanderingProgrammer/render-markdown.nvim',
 }
 
 require('guess-indent').setup()
 require('nvim-autopairs').setup()
+require('render-markdown').setup {
+  completions = { lsp = { enabled = true } },
+  anti_conceal = { enabled = false },
+}
